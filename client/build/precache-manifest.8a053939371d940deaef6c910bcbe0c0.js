a8f7bd8abc30e553e099da6626abc75bcfcec913
Phillip Valdez <Phillip0150@gmail.com>
2019-08-01 22:57:07 -0500

    added build

---
+++ client/build/precache-manifest.8a053939371d940deaef6c910bcbe0c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ada1523996073c10c8ce",
+    "url": "/static/css/main.7c1a1633.chunk.css"
+  },
+  {
+    "revision": "ada1523996073c10c8ce",
+    "url": "/static/js/main.3cfa2819.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7216c8ac3878ede49ca8",
+    "url": "/static/js/2.e954e118.chunk.js"
+  },
+  {
+    "revision": "7db03e2c080fda3a732555c1ecbfb385",
+    "url": "/index.html"
+  }
+];
